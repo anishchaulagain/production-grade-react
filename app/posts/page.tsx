b07e0331c6cc1053c47bc0a6a753b645ce141ec8
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-02-22 21:23:56 +0545

    add posts page.tsx

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,23 @@
+import { usePosts } from '@/features/posts/hooks';
 import React from 'react'
 
 const Posts = () => {
+    const { data, loading, error } = usePosts();
   return (
-    <div>Posts</div>
+    <div>
+        <div>Posts</div>
+        {data && (
+            <div>
+                {data.results.map((result)=>{
+                    return (
+                        <div key={result.id}>{result.title}</div>
+                        
+                    )
+                })}
+            </div>
+        )}
+    </div>
+     
   )
 }
 
--- features/posts/hooks.ts
+++ features/posts/hooks.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PostsResponse } from "./types";
 import { Posts } from "./api";
 
@@ -7,16 +7,19 @@ export const usePosts = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  const fetchPosts = async () => {
-    try {
-      setLoading(true);
-      const response = await Posts.getPosts();
-      setData(response);
-    } catch (err) {
-      setError("Failed to fetch posts");
-    } finally {
-      setLoading(false);
-    }
-  };
-  return { data, loading, error, fetchPosts };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const response = await Posts.getPosts();
+        setData(response);
+      } catch (err) {
+        setError("Failed to fetch posts");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return { data, loading, error };
 };
